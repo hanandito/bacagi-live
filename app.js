c299835e04cc3198a6c03443628a0225e0e6d0dd
teddypicker <hananditos@gmail.com>
2021-05-02 00:27:11 +0700

    initial commit

--- app.js
+++ app.js
@@ -45,3 +45,36 @@
      }
    }
  })
+
+  var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('.counter-wrapper').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.number-counter').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
