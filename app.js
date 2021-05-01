67ad1ad066e4c27a788e489c3d3d15b070984322
teddypicker <hananditos@gmail.com>
2021-05-01 23:21:14 +0700

    intiial commit

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
   //  Favor
+  $('.carousel-banner').addClass('owl-carousel owl-theme').owlCarousel({
+    autoplay:false,
+    navText: ["<img src='chevron-left-square-08.png'>","<img src='chevron-right-square-08.png'>"],
+    autoplayTimeout: 3000,
+    responsive:{
+     0: {
+       items: 1,
+       dots: true,
+       nav: false,
+     },
+     600: {
+       items: 1,
+       nav: false
+     },
+     1000: {
+       items: 1,
+       dots: false,
+       nav: false
+     }
+   }
+ })
   $('.carousel-point').addClass('owl-carousel owl-theme').owlCarousel({
     autoplay:false,
     navText: ["<img src='chevron-left-square-08.png'>","<img src='chevron-right-square-08.png'>"],
